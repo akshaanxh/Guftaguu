7ecc9eed08f2bb31f1ef469ef1485aaca426dfa3
Akshaansh Veer <workwakshaansh@gmail.com>
2026-08-03 13:58:39 +0530

    fix: remove invalid # comment syntax in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,8 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
-// We removed the "@vercel/analytics" lines from here 
-// because we are now using the <Analytics /> component in App.jsx
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
 )
-# FIXME: handle null case properly
-
-# FIXME: improve error message for end user
